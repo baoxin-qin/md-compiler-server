d2e70328717794c3dc85cca558be249e93fda16f
baoxin-qin <165638172+baoxin-qin@users.noreply.github.com>
2026-05-20 16:14:11 +0800

    [dev] 构建最小化 FastAPI 后端项目，编写一键启动脚本

---
+++ scripts/run.ps1
@@ -0,0 +1,2 @@
+echo "Running FastAPI server by uvicorn ..."
+uvicorn src.main:app --host 127.0.0.1 --port 8000
--- src/main.py
+++ src/main.py
@@ -1,1 +1,12 @@
 """项目主程序入口"""
+
+from fastapi import FastAPI
+
+app = FastAPI(title="md-compiler-server", version="0.1.0")
+
+@app.get("/")
+def test_root():
+    return {
+        "code": 200,
+        "msg": "Welcome to FastAPI",
+    }
